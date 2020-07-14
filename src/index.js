620ef26208e0c1e3e4410f5881a2dfadb1d2e5b0
Laura <51352158+dokuritsu@users.noreply.github.com>
2020-07-14 13:17:20 -0500

    added eventlisteners

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var square = document.querySelector("#squares");
+var square = document.getElementById("squares");
+
+square.addEventListener("dblclick", doubleClick);
+
+square.addEventListener("mouseover", hover);
+
+square.addEventListener("mouseup", release);
+
+square.addEventListener("mousedown", hold);
+
+document.body.addEventListener("wheel", scroll);
+
+function scroll() {
+  console.log("Scrolling through the page");
+  square.style.backgroundColor = "darkorange";
+}
+
+function doubleClick() {
+  console.log("Square was double clicked");
+  square.style.backgroundColor = "green";
+}
+
+function hover() {
+  console.log("Mouse is hovering over square");
+  square.style.backgroundColor = "blue";
+}
+
+function release() {
+  console.log("Mouse was released over square");
+  square.style.backgroundColor = "yellow";
+}
+
+function hold() {
+  console.log("Mouse was held over square");
+  square.style.backgroundColor = "red";
+}
